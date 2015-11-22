226a91f9129512cf1287b5d5484b83ad45e2d17d
Ayush <ayush15may@gmail.com>
2015-11-22 17:23:49 +0530

    adding running_time/total_time to the video player1

--- vidPlayer/player_DONE.js
+++ vidPlayer/player_DONE.js
@@ -53,5 +53,24 @@ $(function(){
       $("#load_progress").css({ width: percent + "%" })
     }
   }
+  
+  // Time Display =============================//
+  $(video).bind("timeupdate", function(){
+    $("#current_time").html(formatTime(this.currentTime));
+  });
+  $(video).bind("durationchange", function(){
+    $("#duration").html(formatTime(this.duration));
+  });
+  
+  function formatTime(seconds) {
+    var seconds = Math.round(seconds);
+    var minutes = Math.floor(seconds / 60);
+    // Remaining seconds
+    seconds = Math.floor(seconds % 60);
+    // Add leading Zeros
+    minutes = (minutes >= 10) ? minutes : "0" + minutes;
+    seconds = (seconds >= 10) ? seconds : "0" + seconds;
+    return minutes + ":" + seconds;
+  }
 
 });
