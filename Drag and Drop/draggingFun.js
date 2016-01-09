28b360e2d9438dc696d350ffa83fc682f160b995
Ayush <ayush15may@gmail.com>
2016-01-10 01:35:14 +0530

    displaying thumbnail of uploaded files

--- Drag and Drop/draggingFun.js
+++ Drag and Drop/draggingFun.js
@@ -1,49 +1,31 @@
-function dragLeaveHandler(event) {
-    console.log("drag leave");
-    // Set style of drop zone to default
-    event.target.classList.remove('draggedOver');
-}
+function readFilesAndDisplayPreview(files) {
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
 
-function dragEnterHandler(event) {
-    console.log("Drag enter");
-    // Show some visual feedback
-    event.target.classList.add('draggedOver');
-}
+        // Only process image files.
+        if (!f.type.match('image.*')) {
+            continue;
+        }
 
-function dragOverHandler(event) {
-    //console.log("Drag over a droppable zone");
-    // Do not propagate the event
-    event.stopPropagation();
-    // Prevent default behavior, in particular when we drop images or links
-    event.preventDefault();
-}
+        var reader = new FileReader();
 
-function dropHandler(event) {
-    console.log('drop event');
-
-    // Do not propagate the event
-    event.stopPropagation();
-    // Prevent default behavior, in particular when we drop images or links
-    event.preventDefault();
+        //capture the file information.
+        reader.onload = function(e) {
+            // Render thumbnail.
+            var span = document.createElement('span');
+            span.innerHTML = "<img class='thumb' src='" + e.target.result + "'/>";
+            document.getElementById('list').insertBefore(span, null);
+        };
 
 
-    // reset the visual look of the drop zone to default
-    event.target.classList.remove('draggedOver');
-
-
-    // get the files from the clipboard
-    var files = event.dataTransfer.files;
-    var filesLen = files.length;
-    var filenames = "";
-
-    // iterate on the files, get details using the file API
-    // Display file names in a list.
-    for(var i = 0 ; i < filesLen ; i++) {
-        filenames += '\n' + files[i].name;
-        // Create a li, set its value to a file name, add it to the ol
-        var li =     document.createElement('li');
-        li.textContent = files[i].name;    document.querySelector("#droppedFiles").appendChild(li);
+        // Read the image file as a data URL.
+        reader.readAsDataURL(f);
     }
-    console.log(files.length + ' file(s) have been dropped:\n' + filenames);
-
 }
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+    readFilesAndDisplayPreview(files);
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
