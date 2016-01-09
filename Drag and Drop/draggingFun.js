f63d8fe20ce4faedac61636dd303c4023af4e353
Ayush <ayush15may@gmail.com>
2016-01-09 23:37:59 +0530

    providing more visual feedback using dropEffect property

--- Drag and Drop/draggingFun.js
+++ Drag and Drop/draggingFun.js
@@ -2,6 +2,17 @@
  * Created by Ayush on 1/9/2016.
  */
 function dragStartHandler(event) {
+    // allow copy effect on cursor
+    event.dataTransfer.effectAllowed = 'copy';
+
+    // load and create an image
+    var dragIcon = document.createElement('img');
+    dragIcon.src = 'http://th03.deviantart.net/fs71/150/i/2011/335/0/6/free_html5_3d_logo_icon__10_icons__by_anonsphere-d4h763w.jpg';
+    dragIcon.width = 100;
+
+    // set it to the drag cursor
+    event.dataTransfer.setDragImage(dragIcon, -10, -10);
+
     // Change css class for visual feedback
     event.target.style.opacity = '0.4';
     event.target.classList.add('dragged');
@@ -24,12 +35,14 @@ function dragLeaveHandler(event) {
 
 function dragEnterHandler(event) {
     console.log("Drag enter");
+    event.dataTransfer.dropEffect = 'copy';  // See the section on the DataTransfer object.
     event.target.classList.add('draggedOver');
 }
 
 function dragOverHandler(event) {
     //console.log("Drag over a droppable zone");
     event.preventDefault(); // Necessary. Allows us to drop.
+
 }
 
 function dropHandler(event) {
